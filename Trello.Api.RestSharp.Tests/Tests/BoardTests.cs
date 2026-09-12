75c9bca472f1bfc02197732b3e8a25ef6032eb22
admin <ivasisenvolodimir@gmail.com>
2026-09-12 23:20:46 +0300

    Refactored using DRY method

--- Trello.Api.RestSharp.Tests/Tests/BoardTests.cs
+++ Trello.Api.RestSharp.Tests/Tests/BoardTests.cs
@@ -20,23 +20,26 @@ public class BoardTests : BaseTest
     public async Task CreateBoard_Successfully()
     {
         string boardName =  "AutoTest_Board_" + DateTime.Now.Ticks;
-        var createdBoard = await _boardClient.CreateBoard(boardName);
+        var response = await _boardClient.CreateBoard(boardName);
         
-        _createdBoardId = createdBoard.Id;
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         
-        Assert.That(createdBoard.Name, Is.EqualTo(boardName), "Created board name does not match");
-        Assert.That(createdBoard.Id, Is.Not.Null.And.Not.Empty, "Created board id can not be null or empty");
+        _createdBoardId = response.Data.Id;
         
-        TestContext.WriteLine($"Board created. Name: {createdBoard.Name}, Id: {createdBoard.Id}");
+        Assert.That(response.Data.Id, Is.Not.Null.And.Not.Empty, "Created board id can not be null or empty");
+        Assert.That(response.Data.Name, Is.EqualTo(boardName), "Created board name does not match");
+        
+        TestContext.WriteLine($"Board created. Name: {response.Data.Name}, Id: {response.Data.Id}");
     }
 
     [Test]
     public async Task CreateBoard_WrongToken()
     {
         string boardName =  "AutoTest_Board_Wrong_" + DateTime.Now.Ticks;
-        var wrongBoardStatus = await _boardClient.CreateBoard_WrongToken(boardName);
+        var response = await _boardClient.CreateBoard(boardName, "wrong_token");
         
-        Assert.That(wrongBoardStatus, Is.EqualTo(HttpStatusCode.Unauthorized));
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        Assert.That(response.Data, Is.Null);
         
         TestContext.WriteLine("Success. Board with wrong token was not created");
     }
@@ -44,9 +47,10 @@ public class BoardTests : BaseTest
     [Test]
     public async Task CreateBoard_EmptyName()
     {
-        var wrongBoardStatus = await _boardClient.CreateBoard_EmptyName();
+        var response = await _boardClient.CreateBoard(null);
         
-        Assert.That(wrongBoardStatus, Is.EqualTo(HttpStatusCode.BadRequest));
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.BadRequest));
+        Assert.That(response.Data, Is.Null);
         
         TestContext.WriteLine("Success. Board with empty name was not created");
     }
@@ -55,24 +59,27 @@ public class BoardTests : BaseTest
     public async Task GetBoard_Successfully()
     {
         string boardName =  "AutoTest_Board_" + DateTime.Now.Ticks;
-        var createdBoard = await _boardClient.CreateBoard(boardName);
+        var response = await _boardClient.CreateBoard(boardName);
         
-        _createdBoardId = createdBoard.Id;
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        
+        _createdBoardId = response.Data.Id;
         
         var retrievedBoard = await _boardClient.GetBoard(_createdBoardId);
         
-        Assert.That(retrievedBoard.Name, Is.EqualTo(boardName), "Retrieved board name does not match");
-        Assert.That(retrievedBoard.Id, Is.EqualTo(_createdBoardId), "Retrieved board id does not match");
+        Assert.That(retrievedBoard.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(retrievedBoard.Data.Id, Is.EqualTo(_createdBoardId), "Retrieved board id does not match");
+        Assert.That(retrievedBoard.Data.Name, Is.EqualTo(boardName), "Retrieved board name does not match");
         
-        TestContext.WriteLine($"Board {retrievedBoard.Name} found");
+        TestContext.WriteLine($"Board {retrievedBoard.Data.Name} found");
     }
 
     [Test]
     public async Task GetBoard_WrongId()
     {
-        var wrongRetrievedBoardStatus = await _boardClient.GetBoardWrongId();
+        var response = await _boardClient.GetBoard("123456789012345678901234");
         
-        Assert.That(wrongRetrievedBoardStatus,  Is.EqualTo(HttpStatusCode.NotFound));
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
         
         TestContext.WriteLine("Success. Board with wrong id was not found");
     }
@@ -80,18 +87,21 @@ public class BoardTests : BaseTest
     [Test]
     public async Task UpdateBoard_Successfully()
     {
-        string boardName =  "AutoTest_Board_" + DateTime.Now.Ticks;
-        var createdBoard = await _boardClient.CreateBoard(boardName);
+        string boardName = "AutoTest_Board_" + DateTime.Now.Ticks;
+        var response = await _boardClient.CreateBoard(boardName);
         
-        _createdBoardId = createdBoard.Id;
+        Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
         
-        string newBoardName =  "AutoTest_Board_Changed_" + DateTime.Now.Ticks;
+        _createdBoardId = response.Data.Id;
+        
+        string newBoardName = "AutoTest_Board_Changed_" + DateTime.Now.Ticks;
         var changedBoard = await _boardClient.UpdateBoard(_createdBoardId, newBoardName);
         
-        Assert.That(changedBoard.Name, Is.EqualTo(newBoardName), "Changed board name does not match");
-        Assert.That(changedBoard.Id, Is.EqualTo(_createdBoardId), "Changed board id does not match");
+        Assert.That(changedBoard.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        Assert.That(changedBoard.Data.Id, Is.EqualTo(_createdBoardId), "Changed board id does not match");
+        Assert.That(changedBoard.Data.Name, Is.EqualTo(newBoardName), "Changed board name does not match");
         
-        TestContext.WriteLine($"Board updated. New name: {changedBoard.Name}");
+        TestContext.WriteLine($"Board updated. New name: {changedBoard.Data.Name}");
     }
     
     [TearDown]
@@ -99,8 +109,8 @@ public class BoardTests : BaseTest
     {
         if (!string.IsNullOrEmpty(_createdBoardId))
         {
-            var statusCode = await _boardClient.DeleteBoard(_createdBoardId);
-            if (statusCode == HttpStatusCode.OK)
+            var response = await _boardClient.DeleteBoard(_createdBoardId);
+            if (response.StatusCode == HttpStatusCode.OK)
             {
                 TestContext.WriteLine($"Board {_createdBoardId} deleted");
             }
